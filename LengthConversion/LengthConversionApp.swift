174252e1a12aca408912e7b0df59d30e61fd499c
Thierry <marcelinthierry@gmail.com>
2024-04-14 10:34:27 -0400

    Initial Commit

---
+++ LengthConversion/LengthConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  LengthConversionApp.swift
+//  LengthConversion
+//
+//  Created by Daniel Thierry Marcelin on 2024/4/14.
+//
+
+import SwiftUI
+
+@main
+struct LengthConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
